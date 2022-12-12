5d10a2745de60ab6f95e717318149643aec4f9df
Ximena Politi <ximena.politi@gmail.com>
2022-12-11 19:11:08 -0500

    update game_yankenpo

--- game_yankenpo.py
+++ game_yankenpo.py
@@ -1,22 +1,27 @@
 import random
 
 
-user_option = input('Elige piedra, papel o tijera: ').lower
-options = ('piedra', 'papel', 'tijera')
-assert user_option in options, "Elegir una de las opciones"
-computer_option = random.choice(options)
-print(computer_option)
+def game(user_option, computer_option):
+    my_dict = {
+            "papel": "piedra",
+            "tijera": "papel",
+            "piedra": "tijera"
+            }
+    
+    if user_option == computer_option:
+        print('Empate!')
+    elif user_option == my_dict[computer_option]:
+        print('perdiste!')
+    else:
+        print('ganaste!')
 
-my_dict = {
-        "papel": "piedra",
-        "tijera": "papel",
-        "piedra": "tijera"
-        }
 
-if user_option == computer_option:
-    print('Empate!')
-elif user_option == my_dict[computer_option]:
-    print('perdiste!')
-else:
-    print('ganaste!')
+if __name__ == '__main__':
+    user_option = input('Elige piedra, papel o tijera: ').lower
+    options = ('piedra', 'papel', 'tijera')
+    assert user_option not in options, "Elegir una de las opciones"
+    computer_option = random.choice(options)
+    print(computer_option)
+    game(user_option, computer_option)
+
 
